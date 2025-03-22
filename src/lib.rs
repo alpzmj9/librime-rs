255b0c361042a3c151b09d28fc474e0da47ad37b
Monine Melolaris <alpzmj9@icloud.com>
2025-03-22 16:38:59 +0800

    Update tests.

--- src/lib.rs
+++ src/lib.rs
@@ -6,27 +6,4 @@ pub fn add(left: u64, right: u64) -> u64 {
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-
-    use algo::utilities::compare_version_string;
-    #[test]
-    fn is_compare_version_string_correctly() {
-        assert_eq!(compare_version_string("1", "1"), 0);
-        assert_eq!(compare_version_string("1", "0"), 1);
-        assert_eq!(compare_version_string("0", "1"), -1);
-        assert_eq!(compare_version_string("1.01", "1.001"), 0);
-        assert_eq!(compare_version_string("1", "1.0.0"), 0);
-        assert_eq!(compare_version_string("123", "3"), 1);
-        assert_eq!(compare_version_string("1.10.1.rs", "1.10.1"), 0);
-        assert_eq!(compare_version_string("1.10.1rs", "1.10.1"), 0);
-        assert_eq!(compare_version_string("1.10.1-rs", "1.10c.1"), 0);
-        assert_eq!(compare_version_string("2024-02-05", "0.40"), 1);
-    }
-}
+mod tests;
---
+++ src/tests/mod.rs
@@ -0,0 +1,10 @@
+use super::*;
+
+#[cfg(test)]
+mod algo;
+
+#[test]
+fn it_works() {
+    let result = add(2, 2);
+    assert_eq!(result, 4);
+}
